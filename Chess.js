4f472fab1f2969fe885c5ed225d094b2c62a8b01
Rino <85957627+rinojames007@users.noreply.github.com>
2023-05-29 20:42:54 +0530

    Update Chess.js
    hey, I was just randomly found your repo and thought maybe i can lend an helping hand by writing the logic.

--- Chess.js
+++ Chess.js
@@ -1,1 +1,38 @@
 
+const chessboard = document.getElementById('chessboard');
+
+// Chessboard initialization
+for (let row = 0; row < 8; row++) {
+  for (let col = 0; col < 8; col++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.classList.add((row + col) % 2 === 0 ? 'white' : 'black');
+    cell.dataset.row = row;
+    cell.dataset.col = col;
+    chessboard.appendChild(cell);
+  }
+}
+
+const cells = document.querySelectorAll('.cell');
+let selectedCell = null;
+
+// Cell click event handler
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    if (selectedCell) {
+      if (selectedCell === cell) {
+        selectedCell.classList.remove('selected');
+        selectedCell = null;
+      } else {
+        // you can write your logic here
+        // For simplicity, let's just highlight the selected cell
+        selectedCell.classList.remove('selected');
+        cell.classList.add('selected');
+        selectedCell = cell;
+      }
+    } else {
+      cell.classList.add('selected');
+      selectedCell = cell;
+    }
+  });
+});
