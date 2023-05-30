2e0eabd2d5ba91672fdef33cfd6a5c5a4a601122
RahulMishra0722 <rahulmishra@072002@gmail.com>
2023-05-30 15:34:19 +0530

    i made the pieces move

--- Chess.js
+++ Chess.js
@@ -1,1 +1,23 @@
-
+function init() {
+    const cells = document.querySelectorAll('.square');
+    cells.forEach(el => {
+      el.addEventListener('dragover', ev => ev.preventDefault())
+      el.addEventListener('drop', ev => {
+        const data = ev.dataTransfer.getData("text/plain");
+        ev.target.appendChild(document.getElementById(data));
+      })
+    });
+  
+    const pieces = document.querySelectorAll('.piece');
+    cells.forEach(el => el.addEventListener('dragstart', ev => {
+      ev.dataTransfer.setData("text/plain", ev.target.id);
+      console.log(ev.target);
+      ev.dataTransfer.dropEffect = "move";
+    }));
+  
+  }
+  
+  window.addEventListener("DOMContentLoaded", () => {
+    init();
+  });
+  
